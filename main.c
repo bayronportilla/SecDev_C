ff6013fc141668742c228131eb07d12a09d8542f
bayron portilla revelo <bportilla@bpr.local>
2017-11-05 08:33:19 -0500

    commit

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <gsl/gsl_sf_bessel.h>
 
-int main(void){
-  printf("hello world \n");
+
+int main( int argc, const char *argv[] ) {
+    double x = 5.0 ;
+    double y = gsl_sf_bessel_J0( x ) ;
+    printf("J0(%g) = % .18e\n", x, y ) ;
+    return 0 ;
 }
+
--- makefile
+++ makefile
@@ -1,1 +1,16 @@
+CC = gcc
+CFLAGS = -Wall -I/usr/local/inlcude
+CCFLAGS = -L/usr/local/lib
+TARGET = main
 
+
+$(TARGET):$(TARGET).c
+	$(CC) $(CFLAGS) -c $@.c
+	$(CC) $(CCFLAGS) $@.o -lgsl -lgslcblas -lm -o $@.x
+
+
+clean:
+	rm *#
+
+run:
+	./main.x
